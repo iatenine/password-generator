bf99d9f40d0340e8e83d8633965be4584850f14c
Jack Linhart <iatenine@gmail.com>
2021-07-30 22:34:54 -0600

    Created generatePassword function

--- script.js
+++ script.js
@@ -2,21 +2,58 @@
 const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 const numbers = "0123456789".split("");
 const symbols = "!@#$%^&*()_+-=[]{};:,<.>/?".split("");
+const lengthMsg = `Enter the length of the password (8 - 128 characters): `;
+const charTypeMsg = `Enter the letter of each type you would like your password to contain:\n
+  U: uppercase, L: lowercase, N: numeric, S: special \n
+  * Case and position of letters do not matter`;
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var length = getDesiredLength(
-    "Enter the length of the password (8 - 128 characters): "
-  );
-  console.log("length: ", length);
+  var length = getDesiredLength(lengthMsg);
+  var options = getCharacterTypes(charTypeMsg);
 
-  // passwordText.value = password;
+  passwordText.value = generatePassword(length, options);
 }
 
-// Prompts: character types
+function generatePassword(length, options) {
+  let password = "";
+  let cycle = options.slice();
+
+  for (let i = 0; i < length; i++) {
+    const typeIndex = Math.floor(Math.random() * cycle.length);
+    const type = cycle.splice(typeIndex, 1);
+    if (cycle.length === 0) {
+      cycle = options.slice();
+    }
+    password += getRandomChar(type[0]);
+  }
+
+  return password;
+}
+
+// Get input from the user for the desired character types
+function getCharacterTypes(msg = charTypeMsg) {
+  let types = prompt(msg.toString()).toUpperCase();
+  const permissableTypes = ["U", "L", "N", "S"];
+
+  let selections = []; //This is the array that will actually return
+  for (let i = 0; i < permissableTypes.length; i++) {
+    if (types.includes(permissableTypes[i])) {
+      selections.push(permissableTypes[i]);
+    }
+  }
+
+  // At least one option must be selected
+  if (selections.length === 0) {
+    selections = getCharacterTypes(
+      "Please input at least one valid entry.\n" + charTypeMsg
+    );
+  }
+
+  return selections;
+}
 
 // Prompt for length, calls recursively until a valid length is entered
 function getDesiredLength(msg) {
@@ -35,16 +72,17 @@ function getDesiredLength(msg) {
 // Get a random character of a given type
 function getRandomChar(type) {
   switch (type) {
-    case "lowercase":
+    case "L":
       return getRandomIndex(alphabet);
-    case "uppercase":
+    case "U":
       const uppercaseArr = alphabet.map((letter) => letter.toUpperCase());
       return getRandomIndex(uppercaseArr);
-    case "numeric":
+    case "N":
       return getRandomIndex(numbers);
-    case "special":
+    case "S":
       return getRandomIndex(symbols);
     default:
+      console.error("Invalid type: ", type);
       return getRandomIndex(alphabet);
   }
 }
